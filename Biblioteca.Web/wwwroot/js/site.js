f9eb6efcc20c95e23b872cf4a247364c902d2e2b
Bruno Oliveira <brunooliveira812@gmail.com>
2018-09-14 19:19:51 -0300

    Ajustes no layout das datatables e no javascript.

--- Biblioteca.Web/wwwroot/js/site.js
+++ Biblioteca.Web/wwwroot/js/site.js
@@ -1,7 +1,30 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
 $(document).ready(function () {
-    $('#dtbDados').DataTable();
+    $('.datatable').DataTable(
+        {
+            "pagingType": "simple_numbers",
+            "scrollY": 400,
+            "language": {
+                "lengthMenu": "Exibindo _MENU_ registros por página",
+                "zeroRecords": "A pesquisa não retornou resultados",
+                "info": "Página _PAGE_ de _PAGES_",
+                "infoEmpty": "Sem registros disponíveis",
+                "infoFiltered": "(filtrado de _MAX_ registros)",
+                "infoPostFix": "",
+                "thousands": ".",
+                "loadingRecords": "Carregando...",
+                "processing": "Processando...",
+                "search": "Pesquisar:",
+                "paginate": {
+                    "first": "Primeira",
+                    "last": "Última",
+                    "next": "Próxima",
+                    "previous": "Anterior"
+                },
+                "aria": {
+                    "sortAscending": "Ative para filtrar a coluna ascendentemente",
+                    "sortDescending": "Ative para filtrar a coluna descendentemente"
+                }
+            }
+        }
+    );
 });
